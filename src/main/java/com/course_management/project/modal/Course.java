6c435a7b9a7b3fe0f3b2192873ddf31e56073540
mahiyaniha <nihakh63@gmail.com>
2026-03-29 15:19:33 -0400

    initial commit

---
+++ src/main/java/com/course_management/project/modal/Course.java
@@ -0,0 +1,18 @@
+package com.course_management.project.modal;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "courses")
+@Data
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String code;
+    private String title;
+    private Integer credit;
+    private Integer departmentId;
+}
